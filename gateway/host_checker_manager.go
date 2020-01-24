c217cc310601678d5d60ebc8246bcdfd0c9e518a
Nirav Uchat <nuchat@cisco.com>
2020-01-24 14:39:49 -0800

    Added support for https based host uptime through service discovery

--- gateway/host_checker_manager.go
+++ gateway/host_checker_manager.go
@@ -3,7 +3,6 @@ package gateway
 import (
 	"context"
 	"encoding/base64"
-	"encoding/json"
 	"errors"
 	"net/http"
 	"net/url"
@@ -420,12 +419,14 @@ func (hc *HostCheckerManager) ListFromService(apiID string) ([]HostData, error) 
 
 	// The returned data is a string, so lets unmarshal it:
 	checkTargets := make([]apidef.HostCheckObject, 0)
-	data0, _ := data.GetIndex(0)
-	if err := json.Unmarshal([]byte(data0), &checkTargets); err != nil {
-		log.WithFields(logrus.Fields{
-			"prefix": "host-check-mgr",
-		}).Error("[HOST CHECKER MANAGER] Decoder failed: ", err)
-		return nil, err
+
+	hosts := data.All()
+
+	for _, host := range hosts {
+		h := apidef.HostCheckObject{
+			CheckURL: host,
+		}
+		checkTargets = append(checkTargets, h)
 	}
 
 	hostData := make([]HostData, len(checkTargets))
@@ -535,9 +536,9 @@ func SetCheckerHostList() {
 	apisMu.RLock()
 	for _, spec := range apisByID {
 		if spec.UptimeTests.Config.ServiceDiscovery.UseDiscoveryService {
-			hostList, err := GlobalHostChecker.ListFromService(spec.APIID)
+			newHostDoc, err := GlobalHostChecker.ListFromService(spec.APIID)
 			if err == nil {
-				hostList = append(hostList, hostList...)
+				hostList = append(hostList, newHostDoc...)
 				for _, t := range hostList {
 					log.WithFields(logrus.Fields{
 						"prefix": "host-check-mgr",
@@ -567,6 +568,10 @@ func SetCheckerHostList() {
 		}
 	}
 	apisMu.RUnlock()
+
+	log.WithFields(logrus.Fields{
+		"prefix": "host-check-mgr",
+	}).Info("Final Host Uptime Tracking List", hostList)
 
 	GlobalHostChecker.UpdateTrackingList(hostList)
 }
